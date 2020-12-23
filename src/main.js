934dca24543f80b3adf7ef9e44248932da1ec286
higff <2753108102@qq.com>
2020-12-23 23:15:20 +0800

    第二次提交

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import axios from 'axios'
 import moment from 'moment'
 import qs from 'qs'
-axios.defaults.baseURL = 'http://localhost:5050';
+axios.defaults.baseURL = 'http://127.0.0.1:5050';
 Vue.prototype.axios = axios;
 Vue.prototype.moment = moment;
 Vue.prototype.qs = qs;
